bb01675d95d3b050282ea722d80146899f045a0e
raminarmanfar <ramin.armanfar@msg.group>
2019-10-16 16:52:56 +0200

    bean auto wiring sample done.

--- src/org/ramin/javabrains/Triangle.java
+++ src/org/ramin/javabrains/Triangle.java
@@ -1,22 +1,38 @@
 package org.ramin.javabrains;
 
-import java.util.List;
-
 public class Triangle {
 
-    private List<Point> points;
+    private Point pointA;
+    private Point pointB;
+    private Point pointC;
 
-    public void setPoints(List<Point> points) {
-        this.points = points;
+    public Point getPointA() {
+        return pointA;
     }
 
-    public List<Point> getPoints() {
-        return points;
+    public Point getPointB() {
+        return pointB;
+    }
+
+    public Point getPointC() {
+        return pointC;
+    }
+
+    public void setPointA(Point pointA) {
+        this.pointA = pointA;
+    }
+
+    public void setPointB(Point pointB) {
+        this.pointB = pointB;
+    }
+
+    public void setPointC(Point pointC) {
+        this.pointC = pointC;
     }
 
     public void draw() {
-        for (Point point: points) {
-            System.out.println("Point A = (" + point.getX() + ", " + point.getY() + ")");
-        }
+        System.out.println("Point A = (" + getPointA().getX() + ", " + getPointA().getY() + ")");
+        System.out.println("Point B = (" + getPointB().getX() + ", " + getPointB().getY() + ")");
+        System.out.println("Point C = (" + getPointC().getX() + ", " + getPointC().getY() + ")");
     }
 }
